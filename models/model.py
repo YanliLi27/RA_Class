13cb7100739be41dfd835804d8650ae8c9f34966
yli5 <y.li2@lumc.nl>
2024-02-29 11:59:10 +0100

    update the model, delete softmax

--- models/model.py
+++ models/model.py
@@ -46,20 +46,20 @@ class Classifier(nn.Module):
         self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
         self.classifier_fc = nn.Sequential(
             nn.Linear(256*group_num * 7 * 7, 4096),
-            nn.ReLU(True),
+            nn.SiLU(True),
             nn.Dropout(),
             nn.Linear(4096, 4096),
-            nn.ReLU(True),
+            nn.SiLU(True),
             nn.Dropout(),
             nn.Linear(4096, num_classes),
         )
-        self.softmax = nn.Softmax(dim=1)
+        # self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.classifier_fc(x)
-        x = self.softmax(x)
+        # x = self.softmax(x)
         return x
 
 
